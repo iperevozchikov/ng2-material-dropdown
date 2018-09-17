a1eff94134f160322e9a0e317f000aa709785ea2
Igor Perevozchikov <iperevozchikov@gmail.com>
2018-09-17 15:30:38 +0700

    Test added build

--- dist/ng2-material-dropdown.d.ts
+++ dist/ng2-material-dropdown.d.ts
@@ -1,4 +1,5 @@
 /**
  * Generated bundle index. Do not edit.
  */
-export * from './index';
+//export * from './index';
+export { Ng2Dropdown, Ng2DropdownMenu, Ng2MenuItem, Ng2DropdownButton, Ng2DropdownModule, DropdownStateService } from './src/modules/ng2-dropdown.module';
